0423a8998a1bbbf237279700bda8af91101e4077
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-09-15 08:59:52 +0430

    work with query exp

--- src/View/Cell/InboxCell.php
+++ src/View/Cell/InboxCell.php
@@ -2,6 +2,8 @@
 
 namespace App\View\Cell;
 
+use Cake\Database\Expression\QueryExpression;
+use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
 use Cake\View\Cell;
 
@@ -35,7 +37,13 @@ class InboxCell extends Cell
     public function display()
     {
         $news = TableRegistry::getTableLocator()->get('news');
-        $newsList = $news->find();
+        $newsList = $news->find()
+            ->where(function (QueryExpression $exp, Query $query) {
+//                return $exp->gt('id', 1);
+                $first = $exp->gt('id', 1);
+                $sec = $exp->lt('id', 4);
+                return $exp->and([$first,$sec]);
+            });
         $this->set('newsList', $newsList);
     }
 }
